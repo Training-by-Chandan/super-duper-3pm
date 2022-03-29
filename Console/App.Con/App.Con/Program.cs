bc43d8b774914b2576a91bdf9e32f217c520cbef
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-29 15:37:27 +0545

    if and swicth separated

--- Console/App.Con/App.Con/Program.cs
+++ Console/App.Con/App.Con/Program.cs
@@ -14,44 +14,10 @@ namespace App.Con
             Console.ReadLine();
         }
 
-        //control statements
-        private static void ifElse()
+        private static void SwitchCase()
         {
             Console.WriteLine("Enter a number");
             var num = Convert.ToInt32(Console.ReadLine());
-            if (num == 1)
-            {
-                Console.WriteLine("Sunday");
-            }
-            else if (num == 2)
-            {
-                Console.WriteLine("Monday");
-            }
-            else if (num == 3)
-            {
-                Console.WriteLine("Tuesday");
-            }
-            else if (num == 4)
-            {
-                Console.WriteLine("Wednesday");
-            }
-            else if (num == 5)
-            {
-                Console.WriteLine("Thursday");
-            }
-            else if (num == 6)
-            {
-                Console.WriteLine("Friday");
-            }
-            else if (num == 7)
-            {
-                Console.WriteLine("Saturday");
-            }
-            else
-            {
-                Console.WriteLine("Not a valid day");
-            }
-
             switch (num)
             {
                 case 1:
@@ -89,6 +55,46 @@ namespace App.Con
                 default:
                     break;
             }
+
+            switch (num)
+            {
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("Weekdays");
+                    break;
+
+                case 1:
+                case 7:
+                    Console.WriteLine("Weekends");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        //control statements
+        private static void ifElse()
+        {
+            Console.WriteLine("Enter a number");
+            var num = Convert.ToInt32(Console.ReadLine());
+            if (num == 1) Console.WriteLine("Sunday");
+            else if (num == 2) Console.WriteLine("Monday");
+            else if (num == 3) Console.WriteLine("Tuesday");
+            else if (num == 4) Console.WriteLine("Wednesday");
+            else if (num == 5) Console.WriteLine("Thursday");
+            else if (num == 6) Console.WriteLine("Friday");
+            else if (num == 7) Console.WriteLine("Saturday");
+            else Console.WriteLine("Not a valid day");
+
+            if (num == 2 || num == 3 || num == 4 || num == 5 || num == 6) Console.WriteLine("Weekdays");
+            else if (num == 1 || num == 7) Console.WriteLine("Weekends");
+
+            //ternary: value assign : (condition) ? <true value> : <false value>
+            var days = num == 1 ? "Sunday" : num == 2 ? "Monday" : num == 3 ? "Tuesday" : num == 4 ? "Wednesday" : num == 5 ? "Thursday" : num == 6 ? "Friday" : num == 7 ? "Saturday" : "Not a valid day";
         }
 
         private static void Operators()

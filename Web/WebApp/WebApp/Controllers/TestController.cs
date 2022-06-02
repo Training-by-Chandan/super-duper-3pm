6f2b40be741d434b0e799b9e2aa1de5e1e18f462
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-06-02 08:10:33 +0545

    Web Project added

---
+++ Web/WebApp/WebApp/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult abc()
+        {
+            return View("~/Views/Home/Privacy.cshtml");
+        }
+    }
+}
---
+++ Web/WebApp/WebAppNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppNet.Startup))]
+namespace WebAppNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

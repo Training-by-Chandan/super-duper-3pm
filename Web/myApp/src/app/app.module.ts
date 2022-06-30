f8c1494c6c9457e85991e3b65d2520f98e656166
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-06-30 21:01:42 +0545

    added the components for category

--- Web/myApp/src/app/app.module.ts
+++ Web/myApp/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HeaderComponent } from './main/header/header.component';
 import { SidebarComponent } from './main/sidebar/sidebar.component';
 import { FooterComponent } from './main/footer/footer.component';
 import { BodyComponent } from './main/body/body.component';
+import { ButtonComponent } from './button/button.component';
+import { ListComponent } from './category/list/list.component';
+import { CreateComponent } from './category/create/create.component';
+import { EditComponent } from './category/edit/edit.component';
+import { DeleteComponent } from './category/delete/delete.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +19,11 @@ import { BodyComponent } from './main/body/body.component';
     SidebarComponent,
     FooterComponent,
     BodyComponent,
+    ButtonComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent,
+    DeleteComponent,
 
   ],
   imports: [
--- Web/myApp/src/app/main/body/body.component.ts
+++ Web/myApp/src/app/main/body/body.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-body',
+  selector: 'main[body]',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
